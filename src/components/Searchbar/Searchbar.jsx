b6c0f937a11ce6a601e065854a46a51d9637eab1
Nataliia Smalchenko <yarokrilka@gmail.com>
2024-03-26 02:20:22 +0200

    task completed

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,10 +3,24 @@ import { Component } from 'react';
 import css from './Searchbar.module.css';
 
 class Searchbar extends Component {
+  state = {
+    searchText: '',
+  };
+
+  handleChange = e => {
+    this.setState({ searchText: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.searchText);
+    this.setState({ searchText: '' });
+  };
+
   render() {
     return (
       <header className={css.Searchbar}>
-        <form className={css.SearchForm}>
+        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
           <button type="submit" className={css['SearchForm-button']}>
             🔍
             <span className={css['SearchForm-button-label']}>Search</span>
@@ -15,9 +29,11 @@ class Searchbar extends Component {
           <input
             className={css['SearchForm-input']}
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.searchText}
           />
         </form>
       </header>
